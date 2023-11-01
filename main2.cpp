93cf85480ef92168a29b8d49e48c1b4c684d2479
Gustavo <gustavo100palmeirascosta@hotmail.com>
2023-10-31 22:06:54 -0300

    Cálculo concluído

---
+++ main2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv)
+{
+	float nota1;
+	float nota2;
+	float media;
+	
+	printf("Insira nota 1: ");
+	scanf("%f", &nota1);
+	
+	printf("Insira nota 2: ");
+	scanf("%f", &nota2);
+	
+	media = (nota1 + nota2) / 2;
+	
+	printf("media = %2f", media);
+	
+	
+	return 0;
+}
